d4acd8c365815b54bb10cf771494b8848ea2fd20
ersil <ersil99@gmail.com>
2026-09-26 13:46:14 +0900

    업종 표시

--- tab-cache.test.js
+++ tab-cache.test.js
@@ -11,4 +11,7 @@ try {
 assert.equal(typeof app.buildTabCacheKey, 'function');
 assert.equal(app.buildTabCacheKey('한국', '일봉'), '한국|일봉');
 assert.equal(app.buildTabCacheKey('미국', '주봉'), '미국|주봉');
+assert.equal(app.getIndustryColumn(['종목코드', '종목명', '등락률', '업종']), 3);
+assert.equal(app.getIndustryColumn(['Code', 'Name', 'Change', '산업 구분']), 3);
+assert.equal(app.getIndustryColumn(['종목코드', '종목명']), -1);
 console.log('tab cache helper ok');
